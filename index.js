a6a7b6a71df6a2dc6f2065490feb6b3e55f760be
Fernando Cristobal De La Cruz <fernandocgomez@live.com>
2019-08-22 14:35:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,9 @@ function theBeatlesPlay(musicians, instruments) {
 
 function iLoveTheBeatles(num) {
   var array = [];
-  
+  do {
+    array.push("I love the Beatles!")
+    num++;
+  } while (num < 15);
+  return array;
 }
